4da8ac84b9aaa977e0c00e19beb8f9d2ddd806fa
IGerrebrands <gerrebrands.i@gmail.com>
2017-01-10 16:35:24 +0100

    init

---
+++ app/app.js
@@ -0,0 +1,40 @@
+const rp = require('request-promise');
+const feeds = [];
+
+function writeToDoc(feed){
+	document.querySelector('.buy').innerHTML = feed.B;
+	document.querySelector('.sell').innerHTML = feed.S;
+	document.querySelector('.percentage').innerHTML = feed.P + "%";
+	const label = document.querySelector('.rising');
+	if(feed.rising) {
+		label.innerHTML = "Rising";
+		label.style.color = "green";
+	} else {
+		label.innerHTML = "Dropping";
+		label.style.color = "red";
+	}
+	document.querySelector('.date').innerHTML = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+}
+
+setInterval(function () {
+	rp('http://marketools.plus500.com/Feeds/UpdateTable?instsIds=123')
+	.then(function (res) {
+		let data = JSON.parse(res);
+        let feed = data.Feeds[0];
+		if(feeds.length === 0) {
+			feeds.push(feed);
+			writeToDoc(feed);
+			return;
+		}
+        let oldFeed = feeds[feeds.length-1];
+		if(oldFeed.B !== feed.B && oldFeed.S !== feed.B && oldFeed.B !== feed.P) {
+			feed.rising = feed.P > oldFeed.P;
+			writeToDoc(feed);
+			feeds.push(feed);
+		}
+	})
+	.catch(function (err) {
+		console.log(err);
+	});
+
+}, 1000);
