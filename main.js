4f844e660c6d267199c5ec9586cddd1154821427
IGerrebrands <gerrebrands.i@gmail.com>
2017-01-11 14:25:52 +0100

    build script fix

--- main.js
+++ main.js
@@ -1,40 +1,36 @@
-'use strict';
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
-var _require = require('electron'),
-    app = _require.app,
-    BrowserWindow = _require.BrowserWindow;
-
-var path = require('path');
-var url = require('url');
-
-var win = void 0;
+let win;
 
 function createWindow() {
-		win = new BrowserWindow({ width: 800, height: 500 });
+    win = new BrowserWindow({width: 800, height: 500});
 
-		win.loadURL(url.format({
-				pathname: path.join(__dirname, './app/index.html'),
-				protocol: 'file:',
-				slashes: true
-		}));
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, './app/index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-		win.webContents.openDevTools();
+    win.webContents.openDevTools();
 
-		win.on('closed', function () {
-				win = null;
-		});
+    win.on('closed', () => {
+        win = null
+    });
+
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
-		if (process.platform !== 'darwin') {
-				app.quit();
-		}
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-app.on('activate', function () {
-		if (win === null) {
-				createWindow();
-		}
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
 });
