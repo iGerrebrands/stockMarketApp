2f57dc266299e5ce94a3b51463b1e684226b19a7
IGerrebrands <gerrebrands.i@gmail.com>
2017-01-11 14:56:50 +0100

    Created data visualisation to display htpp request data

--- app/overview/Overview.js
+++ app/overview/Overview.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import getStockPriceHTTP from '../models/getStockPriceHTTP';
 
 export default class Overview extends React.Component {
 
     constructor() {
         super();
+        this.state = { data: "test" };
+        setInterval(() => {
+            this.updateLoop();
+        }, 1000);
+    }
+
+    updateLoop() {
+        getStockPriceHTTP("123").then((res) => {
+            const data = JSON.parse(res);
+            this.setState({data: data});
+            console.log(data.Feeds[0]);
+        });
     }
 
     render() {
         return (
             <div>
-                <h1>OVERVIEW</h1>
+                <h1>{ JSON.stringify(this.state.data) }</h1>
             </div>
         );
     }
